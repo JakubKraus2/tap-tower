4b1219cb9100222164b05b4a7c751785cff046e0
jakub.kraus2 <jakub.kraus007@seznam.cz>
2023-08-12 23:19:21 +0200

    maybe better optimalization

--- game/particles&effects/LightTrail.gd
+++ game/particles&effects/LightTrail.gd
@@ -9,7 +9,18 @@ var queue : Array
 func _ready():
 	set_modulate(Color(randf_range(0.2, 1), randf_range(0.2, 1), randf_range(0.2, 1)))
 
-func _process(delta):
+#func _process(delta):
+#	var pos = player.position
+#	queue.push_front(pos)
+#
+#	if queue.size() > max_lenght:
+#		queue.pop_back()
+#	clear_points()
+#
+#	for point in queue:
+#		add_point(point)
+
+func _on_create_point_timer_timeout():
 	var pos = player.position
 	queue.push_front(pos)
 	
