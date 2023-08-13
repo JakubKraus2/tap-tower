7867acf4510b3cbdfac70f5dc4b6787e08e3c919
jakub.kraus2 <jakub.kraus007@seznam.cz>
2023-08-13 02:36:07 +0200

    game over screen concept

---
+++ game/menus/game_over_menu.gd
@@ -0,0 +1,15 @@
+extends CanvasLayer
+
+
+
+
+func _ready():
+	$Border/HBoxContainer/ScoreContainer/Score.text = "Score \n" + str(GlobalValues.score)
+	$Border/HBoxContainer/ScoreContainer/HighScore.text = "High Score \n" +  str(GlobalValues.high_score)
+
+
+func _on_restart_button_pressed():
+	get_tree().reload_current_scene()
+
+func _on_quit_button_pressed():
+	get_tree().quit()
--- game/menus/start_menu.gd
+++ game/menus/start_menu.gd
@@ -8,3 +8,4 @@ func _input(event):
 	if event.is_pressed():
 		$AnimationPlayer.play("despawn")
 		$"../PlatformTest".set_process_mode(Node.PROCESS_MODE_INHERIT)    
+		Score.visible = true
