e43edba4c65ccfbfcb6c704a1ef403b7961e1fc5
jakub.kraus2 <jakub.kraus007@seznam.cz>
2023-08-14 13:26:08 +0200

    resume countdown after unpausing

--- game/menus/pause_menu.gd
+++ game/menus/pause_menu.gd
@@ -6,12 +6,15 @@ extends CanvasLayer
 func _ready():
 	$PauseButton.visible = true
 	$PauseSymbol.visible = false
+	$CountDown.visible = false
 
 func _input(event):
 	if event.is_pressed():
-		get_tree().paused = false
 		$PauseButton.visible = true
 		$PauseSymbol.visible = false
+		$AnimationPlayer.play("countdown")
+		await $AnimationPlayer.animation_finished
+		get_tree().paused = false
 
 
 func _on_pause_button_pressed():
