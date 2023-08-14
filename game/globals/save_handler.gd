fb65f7fdb922e4b63d3a84f557854c1182d1c497
jakub.kraus2 <jakub.kraus007@seznam.cz>
2023-08-14 18:57:07 +0200

    mute button

--- game/globals/save_handler.gd
+++ game/globals/save_handler.gd
@@ -19,10 +19,12 @@ func save_data():
 func load_data():
 	if !FileAccess.file_exists(SAVE_PATH):
 		data = {
-			"high_score": GlobalValues.high_score
+			"high_score": GlobalValues.high_score, #0
+			"sound": AudioServer.is_bus_mute(0) #false
 		}
 		save_data()
 	var file = FileAccess.open(SAVE_PATH, FileAccess.READ)
 	data = file.get_var()
 	GlobalValues.high_score = data.high_score
+	AudioServer.set_bus_mute(0, data.sound)
 	file.close()
--- game/menus/pause_menu.gd
+++ game/menus/pause_menu.gd
@@ -7,17 +7,23 @@ func _ready():
 	$PauseButton.visible = true
 	$PauseSymbol.visible = false
 	$CountDown.visible = false
-
-func _input(event):
-	if event.is_pressed() && !$AnimationPlayer.is_playing():
-		$PauseSymbol.visible = false
-		$AnimationPlayer.play("countdown")
-		await $AnimationPlayer.animation_finished
-		$PauseButton.visible = true
-		get_tree().paused = false
+	$MusicButton.button_pressed = SaveHandler.data.sound
 
 
 func _on_pause_button_pressed():
 	get_tree().paused = true
 	$PauseButton.visible = false
 	$PauseSymbol.visible = true
+
+func _on_music_button_toggled(button_pressed):
+	AudioServer.set_bus_mute(0, button_pressed)
+	SaveHandler.data.sound = button_pressed
+	SaveHandler.save_data()
+
+func _on_resume_button_pressed():
+	if !$AnimationPlayer.is_playing():
+		$PauseSymbol.visible = false
+		$AnimationPlayer.play("countdown")
+		await $AnimationPlayer.animation_finished
+		$PauseButton.visible = true
+		get_tree().paused = false
